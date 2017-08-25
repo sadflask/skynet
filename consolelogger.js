829e7037554b8842b6f2d8729110c6215dac77bc
sadflask <benjamin.n.frew@gmail.com>
2017-08-25 17:40:04 +1200

    Added logging of JSON objects

--- consolelogger.js
+++ consolelogger.js
@@ -12,10 +12,18 @@ logError(err);
 */
 
 exports.logMessage = function logMessage(message) {
-    
+
     console.log('========== START LOGGING MESSAGE ==========');
     console.log(message);
     console.log('=========== END LOGGING MESSAGE ===========');
+};
+
+exports.logObj = function logobj(obj) {
+
+    console.log('========== START LOGGING OBJECT ==========');
+    console.log(JSON.stringify(obj));
+
+    console.log('=========== END LOGGING OBJECT ===========');
 };
 
 exports.logError = function logError(err) {
