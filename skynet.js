4defccb1a11d64afc9bcf02e611effd7c1f75f0c
sadflask <benjamin.n.frew@gmail.com>
2017-08-26 12:07:25 +1200

    Added limit on stats display

--- skynet.js
+++ skynet.js
@@ -3,13 +3,17 @@ var Discord = require('discord.js');
 var auth = require('./auth.json');
 var logger = require('./consolelogger');
 var utils = require('./utils');
+var printer = require("./print.js");
 
 // Initialize Discord Bot
 var bot = new Discord.Client({});
 
+var timeOfPrint = Date.now();
+
 bot.on('ready', function (evt) {
     logger.logMessage('Connected');
     logger.logMessage('Logged in as: '+bot.user.username + ' - (' + bot.user.id + ')');
+
 });
 bot.on('message', function(msg) {
     // Our bot needs to know if it will execute a command
@@ -26,6 +30,14 @@ bot.on('message', function(msg) {
             break;
             case 'uptime':
                 msg.channel.send(utils.getUptime());
+            break;
+            case 'stats':
+                if ((Date.now()-timeOfPrint)>1000*60*5) {
+                msg.channel.send('I will now print the stats');
+                timeOfPrint = Date.now();
+              } else {
+                msg.channel.send('Too soon!');
+              }
             break;
             // Just add any case commands if you want to..
          }
